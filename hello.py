1bae8354c36043e227b4fbfdaf0578cf4e632df6
Theodore Shiao <theo.shiao@gmail.com>
2018-11-03 04:07:42 -0700

    test

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from nba_api.stats.endpoints import commonplayerinfo
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    player_info = commonplayerinfo.CommonPlayerInfo(player_id=2544)
+    lebron_dict = player_info.common_player_info.get_dict()
+    return "Name:" + lebron_dict.get('data')[0][3] + " , Age: " + lebron_dict.get('data')[0][13]
