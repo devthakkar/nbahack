85c24d4c281bf8e534bdb99658607c0a9d50af06
Nilay Neeranjun <nneeranjun@Nilays-MacBook-Pro.local>
2018-11-05 13:20:48 -0800

    Removed old button code

--- js/choose_player.js
+++ js/choose_player.js
@@ -1,13 +1,4 @@
 $(document).ready(function(){
-    var choosePlayer = function(data) {
-        var player = data.target.innerHTML;
-        let url = `./player.html?playerName=${player}`
-        window.location = url;
-    };
-    var spanSelector= document.querySelectorAll('#button');
-    for (var i = 0; i <  spanSelector.length;  i++) {
-        spanSelector[i].onclick= choosePlayer;
-    }
     $.get("/allplayers", function(data) {
         var names = [];
         for (var i = 0; i < data.length; i++) {
